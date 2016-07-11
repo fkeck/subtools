6ded4d0949f257991a6151105ac1e75cfbb237db
fkeck <francois.keck@gmail.com>
2016-07-12 00:15:11 +0200

    minifix

--- R/clean_subtitles.R
+++ R/clean_subtitles.R
@@ -1,16 +1,15 @@
-# Utilities functions
 
 cleanTags <- function(x, format = "srt"){
-  
+
   format <- match.arg(format, choices = c("srt", "sub", "ssa", "ass", "webvtt", "all"), several.ok = FALSE)
-  
+
   if(format %in% c("srt", "all")){
     x <- gsub("<.+?>", "", x)
   }
-  
+
   if(format %in% c("ass", "ssa", "all")){
     x <- gsub("\\{\\\\.+?\\}", "", x)
   }
-  
+
   return(x)
 }
