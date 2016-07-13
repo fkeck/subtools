688094b729b27745aa14930b8bc6da19d85b906d
fkeck <francois.keck@gmail.com>
2016-07-13 12:37:02 +0200

    implement collapse and sequential

--- R/utils_subtitles.R
+++ R/utils_subtitles.R
@@ -1,28 +1,34 @@
 combineSubs <- function(..., collapse = TRUE, sequential = TRUE){
-  sl <- list(...)
-  k <- list()
-    for(i in 1:length(sl)){
-    if(is(sl[[i]], "MultiSubtitles")){
-      k <- append(k, unlist(sl[i], recursive = FALSE))
+  input <- list(...)
+  sl <- list()
+    for(i in 1:length(input)){
+    if(is(input[[i]], "MultiSubtitles")){
+      sl <- append(sl, unlist(input[i], recursive = FALSE))
     } else {
-      k <- append(k, sl[i])
+      sl <- append(sl, input[i])
     }
   }
-  sl <- k
-  id.max <- 0
-  tcout.max <- "00:00:00.000"
-  for(i in 1:length(sl)){
-    sl[[i]][ , "ID"] <- as.numeric(sl[[i]][ , "ID"]) + id.max
-    id.max <- max(sl[[i]][ , "ID"])
 
-    sl[[i]][ , "Timecode.in"] <- sapply(sl[[i]][ , "Timecode.in"], .add_timecodes,
-                                        y = tcout.max, USE.NAMES = FALSE)
-    sl[[i]][ , "Timecode.out"] <- sapply(sl[[i]][ , "Timecode.out"], .add_timecodes,
-                                        y = tcout.max, USE.NAMES = FALSE)
-    tcout.max <- max(sl[[i]][ , "Timecode.out"])
+  if(sequential){
+    id.max <- 0
+    tcout.max <- "00:00:00.000"
+    for(i in 1:length(sl)){
+      sl[[i]][ , "ID"] <- as.numeric(sl[[i]][ , "ID"]) + id.max
+      id.max <- max(sl[[i]][ , "ID"])
+  
+      sl[[i]][ , "Timecode.in"] <- sapply(sl[[i]][ , "Timecode.in"], .add_timecodes,
+                                          y = tcout.max, USE.NAMES = FALSE)
+      sl[[i]][ , "Timecode.out"] <- sapply(sl[[i]][ , "Timecode.out"], .add_timecodes,
+                                          y = tcout.max, USE.NAMES = FALSE)
+      tcout.max <- max(sl[[i]][ , "Timecode.out"])
+    }
+  }  
+  
+  if(collapse){
+    sl <- do.call("rbind", sl)
   }
-  res <- do.call("rbind", sl)
-  return(res)
+  
+  return(sl)
 }
 
-sl <- list(a, b)
+tail(combineSubs(a))
