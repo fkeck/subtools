28b71b882af332802d71ecf936620099b6e1f7e0
fkeck <francois.keck@gmail.com>
2016-07-20 16:39:51 +0200

    Adapt rawText to the new class Subtitles

--- R/coerce_subtitles.R
+++ R/coerce_subtitles.R
@@ -1,11 +1,11 @@
 
 rawText <- function(x){
   if(inherits(x, "Subtitles")){
-    res <- x[ , "Text"]
+    res <- x$subtitles$Text
     res <- paste(res, collapse = " ")
   }
   if(inherits(x, "MultiSubtitles")){
-    res <- lapply(x, function(x) x[ , "Text"])
+    res <- lapply(x, function(x) x$substitles$Text)
     res <- do.call("c", res)
     res <- paste(res, collapse = " ")
   }
