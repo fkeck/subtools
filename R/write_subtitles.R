d55543e2084226337d0e21a722dfea89134b3b38
fkeck <francois.keck@gmail.com>
2016-07-25 11:23:20 +0200

    add doc

--- R/write_subtitles.R
+++ R/write_subtitles.R
@@ -1,4 +1,13 @@
-
+#' Write subtitles
+#' 
+#' This function writes a \code{Subtitles} object in a file.
+#'
+#' @param x an object of class \code{Subtitles}.
+#' @param file a character string naming a file for writing.
+#' @param format a character string giving the file format. Not used (only SubRip format is implemented).
+#'
+#' @export
+#'
 write.subtitles <- function(x, file, format = "srt"){
   
   if(!is(x, "Subtitles")){
