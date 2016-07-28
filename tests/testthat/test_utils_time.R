dd9a014db591887791a552bb63b170d5f2864ce7
fkeck <francois.keck@gmail.com>
2016-07-28 17:02:13 +0200

    update timecodes tests

--- tests/testthat/test_utils_time.R
+++ tests/testthat/test_utils_time.R
@@ -20,3 +20,13 @@ test_that(".format_subtime guaranties a fixed length output", {
   expect_equal(nchar(.format_subtime("1:2:3.123")), 12)
   expect_equal(nchar(.format_subtime("1")), 12)
 })
+
+
+test_that(".add_timecodes : various additions", {
+  expect_equal(.add_timecodes("00:00:00.000", "00:00:00.000"), "00:00:00.000")
+  expect_equal(.add_timecodes("01:02:03.004", "01:02:03.004"), "02:04:06.008")
+  expect_equal(.add_timecodes("00:00:00.999", "00:00:00.001"), "00:00:01.000")
+  expect_equal(.add_timecodes("00:59:59.999", "00:00:00.001"), "01:00:00.000")
+  expect_equal(.add_timecodes("99:00:00.000", "01:00:00.000"), "100:00:00.000")
+})
+
