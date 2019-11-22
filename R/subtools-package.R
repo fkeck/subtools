97c1a69073ee8819e0e1206bfbde8f5178c5b182
fkeck <francois.keck@gmail.com>
2019-11-22 16:32:53 +0100

    Add imports

--- R/subtools-package.R
+++ R/subtools-package.R
@@ -5,13 +5,15 @@
 #' "ssa", "ass", "substation", "vtt", and "webvtt".
 #'
 #' @keywords internal
-#' @importFrom tm Corpus VectorSource
 #' @importFrom methods is
 #' @importFrom utils read.csv
 #' @importFrom jsonlite fromJSON
 #' @importFrom tibble as_tibble
 #' @importFrom dplyr bind_rows bind_cols
 #' @importFrom readr guess_encoding
+#' @importFrom hms hms
+#' @importFrom tidytext unnest_tokens
+#' @importFrom rlang .data as_label
 #'
 #' @name subtools
 "_PACKAGE"
