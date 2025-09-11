7939b8b541b28e0362ab952281a1f2e4e8d386e7
AlbanSagouis <sagouis@pm.me>
2025-09-11 22:28:19 +0200

    testthat edition 3 - test suite update

--- tests/testthat/test_as.R
+++ tests/testthat/test_as.R
@@ -1,20 +1,21 @@
-test_that("as coercion works", {
-
+test_that("as coercion works as expected", {
   as_subtitle(
-    c("WEBVTT",
+    c(
+      "WEBVTT",
       "X-TIMESTAMP-MAP=MPEGTS:181083,LOCAL:00:00:00.000",
       "",
       "3",
-      "00:00:21.199 --> 00:00:22.333", ">> FEMALE SPEAKER:",
+      "00:00:21.199 --> 00:00:22.333",
+      ">> FEMALE SPEAKER:",
       "Don't stay up too late.",
       "",
       ""
-    ), format = "webvtt"
+    ),
+    format = "webvtt"
   ) -> st
 
   expect_equal(
     st[["Text_content"]],
     ">> FEMALE SPEAKER: Don't stay up too late."
   )
-
 })
--- tests/testthat/test_unnest_tokens.R
+++ tests/testthat/test_unnest_tokens.R
@@ -7,6 +7,7 @@ test_that("unnest_token uses the proper method for subtitles class object", {
 })
 test_that("unnest_token.subtitles works as expected", {
   skip_on_ci()
+  skip_on_cran()
   f <- system.file("extdata", "ex_webvtt.vtt", package = "subtools")
   s <- read_subtitles(f, metadata = data.frame(test = "Test"))
   testthat::expect_snapshot(
